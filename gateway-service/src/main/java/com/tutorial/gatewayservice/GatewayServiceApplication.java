d7712efe050dfe0e882eab78f91ae5433864bbd4
Ignacio Andrés Ávila Moreno <ignacio.avila@usach.cl>
2025-06-10 04:13:40 -0400

    Update gateway

--- gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
@@ -2,14 +2,27 @@ package com.tutorial.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOriginPattern("*");   
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsWebFilter(source);
+	}
 }
