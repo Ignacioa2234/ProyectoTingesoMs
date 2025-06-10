093bcb29971164a9eaa891a14016306935c08472
Ignacio Andrés Ávila Moreno <ignacio.avila@usach.cl>
2025-06-10 04:20:14 -0400

    Update proyecto

--- gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/tutorial/gatewayservice/GatewayServiceApplication.java
@@ -18,7 +18,7 @@ public class GatewayServiceApplication {
 	public CorsWebFilter corsWebFilter() {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.addAllowedOriginPattern("*");   
+		config.addAllowedOriginPattern("*");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
